e15449772dc8e41b904704f0c52c18291e6ae75c
nobulp <lucaslupon873@gmail.com>
2026-01-22 20:33:54 +0000

    fix: removing the gestion of index Error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (TypeError, ValueError):
             continue
-        except IndexError:
-            break
+
     print()
     return count
