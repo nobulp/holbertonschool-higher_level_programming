200cd70e7a204706bd0d13e9235ef9fb34d8061b
nobulp <lucaslupon873@gmail.com>
2026-01-22 20:03:26 +0000

    fix: Refactor safe_print_list to improve variable naming and error handling

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
+    printed = 0
+
+    for idx in range(x):
         try:
-            print(my_list[i], end="")
-        except (IndexError, TypeError):
+            value = my_list[idx]
+            print("{}".format(value), end="")
+            printed += 1
+        except IndexError:
             break
-        else:
-            count += 1
+
     print()
-    return count
+    return printed
