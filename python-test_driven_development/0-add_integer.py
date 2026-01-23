cc462eef843dbd5874971be33074253f1af87c9f
nobulp <lucaslupon873@gmail.com>
2026-01-23 09:27:10 +0000

    fix: Remove NaN and infinity checks from add_integer function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,10 +13,4 @@ def add_integer(a, b=98):
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
-    # Reject NaN and infinity
-    if a != a or a in (float("inf"), float("-inf")):
-        raise TypeError("a must be an integer")
-    if b != b or b in (float("inf"), float("-inf")):
-        raise TypeError("b must be an integer")
-
     return int(a) + int(b)
