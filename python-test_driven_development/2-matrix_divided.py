c7d5d75a01d0b7d11d7895fc2b0dc6feca35eac0
nobulp <lucaslupon873@gmail.com>
2026-01-23 10:43:58 +0000

    feat: Add a script that divides all elements of a matrix.

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -29,7 +29,4 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    if div == float("inf"):
-        return [[0.0 for _ in row] for row in matrix]
-
     return [[round(elem / div, 2) for elem in row] for row in matrix]
