9a78cdf7d5d3fbf3ef0835e712714df79a3ca91d
nobulp <lucaslupon873@gmail.com>
2026-01-23 11:33:17 +0000

    feat: Add a function that prints my name is... with doctest

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module provides a function that prints a person's name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints 'My name is <first name> <last name>'.
+
+    Args:
+        first_name: The first name (must be a string)
+        last_name: The last name (must be a string, default is "")
+
+    Raises:
+        TypeError: If first_name or last_name is not a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
