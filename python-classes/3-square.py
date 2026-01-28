1cfb48b11e390d0e3c1e36619b8f400036547077
nobulp <lucaslupon873@gmail.com>
2026-01-28 16:38:59 +0100

    feat: Adding a new line at the end of the file

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,7 +15,7 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than zero.
         """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
 
         if size < 0:
