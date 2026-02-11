2795c9f04e0f385529b80535a346b74d95226fcc
nobulp <lucaslupon873@gmail.com>
2026-02-11 23:47:03 +0100

    fix: Changing the format of the code to fit pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Module for reading and printing text files."""
+
 def read_file(filename=""):
-    """Read a UTF-8 text file and print it to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
