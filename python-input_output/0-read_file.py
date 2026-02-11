8c8a8c0c68e841354d525e391af5ea32acf36925
nobulp <lucaslupon873@gmail.com>
2026-02-11 23:48:13 +0100

    fix: Add a blank line for improved readability in read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for reading and printing text files."""
 
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, "r", encoding="utf-8") as file:
