5776068c861b26d4cea843f9e79e36ad0f655807
Tnobulp <lucaslupon873@gmail.com>
2026-03-06 12:50:38 +0000

    fix: Adding utf8

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-"""Display states whose name matches the provided argument."""
-
-import sys
+"""Script that filters states by user input from the database."""
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-    )
-
-    cursor = conn.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4]
-    )
-    cursor.execute(query)
-
-    for state in cursor.fetchall():
-        print(state)
-
-    cursor.close()
-    conn.close()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                .format(sys.argv[4]))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
