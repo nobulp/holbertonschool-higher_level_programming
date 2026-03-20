36089c6434a63eb5ecf1ab04bfa442f124f3acc8
nobulp <lucaslupon873@gmail.com>
2026-03-20 13:45:32 +0100

    feat: add 11-second_biggest.js script

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const numbers = process.argv.slice(2).map(Number);
+
+if (numbers.length < 2) {
+  console.log(0);
+} else {
+  numbers.sort((a, b) => b - a);
+  console.log(numbers[1]);
+}
